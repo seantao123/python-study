d99e69a34ece4c12e55e17f828824d4f00f17450
sean.t <mega.reinforcement@gmail.com>
2024-05-23 14:13:09 -0700

    python day 3

--- day 3/python3.py
+++ day 3/python3.py
@@ -1,45 +1,54 @@
-def triangle(rows):
-    for a in range(rows):
-        As = 2 * a + 1
-        space = rows - a - 1
-        row = ' ' * space + 'A' * As
-        print(row)
-
-
+def triangle(z):
+    for x in range(z):
+        A = 2 * x + 1
+        S = z - x - 1
+        T = S * ' ' + A * 'A'
+        print(T)
 triangle(4)
 
+def flip_triangle(z):
+    for x in range(z):
+        A = 2 * (z - x - 1) + 1
+        S = x
+        T = S * ' ' + A * 'A'
+        print(T)
+flip_triangle(4)
 
-def flippedTriangle(rows):
-    for a in range(rows):
-        As = 2 * (rows - a - 1) + 1
-        space = a
-        row = ' ' * space + 'A' * As
-        print(row)
+def right_triangle(z):
+    for x in range(z):
+        A = x + 1
+        S = z - x
+        T = S * ' ' + A * 'A'
+        print(T)
+    for y in range(z - 1):
+        a = z - y - 1
+        s = z - a + 1
+        t = s * ' ' + a * 'A'
+        print(t)
+right_triangle(4)
 
+def left_triangle(z):
+    for x in range(z):
+        A = x + 1
+        S = 3 - x
+        T = A * 'A' + S * ' '
+        print(T)
+    for y in range(z - 1):
+        a = z - y - 1
+        s = - 1 * y + 1
+        t = a * 'A' + s * ' '
+        print(t)
+left_triangle(4)
 
-flippedTriangle(4)
-
-
-def Right_side_triangle(rows):
-    totalRows = 2 * rows - 1
-    for a in range(totalRows):
-        if a < rows:
-            As = a + 1
-        else:
-            As = (totalRows - a - 1) + 1
-        row = 'A' * As
-        print(row)
-
-
-Right_side_triangle(4)
-
-
-def Left_side_triangle(rows):
-    for a in range(rows * 2 - 1):
-        space = abs(rows - a - 1)
-        As = rows - space
-        row = ' ' * space + 'A' * As
-        print(row)
-
-
-Left_side_triangle(4)
+def diamond(z):
+    for x in range(z - 1):
+        A = 2 * x + 1
+        S = z - x - 1
+        T = S * ' ' + A * 'A'
+        print(T)
+    for x in range(z):
+        A = 2 * (z - x - 1) + 1
+        S = x
+        T = S * ' ' + A * 'A'
+        print(T)
+diamond(4)
